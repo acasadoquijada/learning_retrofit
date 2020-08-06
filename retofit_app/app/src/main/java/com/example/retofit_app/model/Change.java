4523c43d70afdc7f6e62a8c0005a34264f49cc73
acasadoquijada <acasadoquijada@gmail.com>
2020-08-06 11:39:18 +0200

    feat: basic retrofit example
    The application uses retrofit to gather information of open issues from
    the https://git.eclipse.org/r/ webpage.

---
+++ retofit_app/app/src/main/java/com/example/retofit_app/model/Change.java
@@ -0,0 +1,15 @@
+package com.example.retofit_app.model;
+
+public class Change {
+
+    String subject;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+}
